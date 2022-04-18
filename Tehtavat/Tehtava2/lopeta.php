a62bbab261b96eb7e9c79b0e0966cecff5fede9e
Ismo Manninen <ismo.manninen@edu.lapinamk.fi>
2022-04-18 21:39:55 +0300

    last fix

--- Tehtavat/Tehtava2/lopeta.php
+++ Tehtavat/Tehtava2/lopeta.php
@@ -1,19 +1,13 @@
 <?php
-/*
-    file:   lopeta.php
-    desc:   Hävittää kaikki sovelluksen tilatiedot eli cookiet ja mahdolliset session-tiedot
-*/
-setcookie('luku1','',-60); //vanhenemisaika negatiivinen -> selain hävittää cookien
+setcookie('luku1','',-60);
 setcookie('operaattori','',-60);
 setcookie('luku2','',-60);
 setcookie('lukuKertotaulu','',-60);
 setcookie('nimi','',-60);
 setcookie('ika','',-60);
 
-//session-tiedot myös
-session_start(); //käynnistää tai ottaa käyttöön olemassa olevan session 
-session_destroy(); //hävitetään nykyinen sessio
+session_start(); 
+session_destroy(); 
 
-//ohjataan alkuun
 header('location: php-perusteita-osa-2.php');
 ?>
